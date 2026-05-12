493a8df56a59913babc3e07641dc332bcc0753c3
torredds <eivanflorest@gmail.com>
2026-05-11 18:23:10 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+// Service Worker para Tracker PWA
+const CACHE_NAME = 'tracker-v1';
+const URLS = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(URLS)).catch(() => {}));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      if (res && res.status === 200 && e.request.method === 'GET') {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(c => c.put(e.request, clone)).catch(() => {});
+      }
+      return res;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
